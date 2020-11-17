bcbb3ba20dd1fcc12232d3da91cf3633e45fcb3b
Ioan Ojica <ivan.andrei93@mail.ru>
2020-11-17 12:08:52 +0100

    controller and views for User created

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,8 @@ ActiveRecord::Schema.define(version: 2020_11_16_153132) do
     t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_11_16_153132) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
