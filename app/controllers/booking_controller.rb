294c435f92c6c48fd88e44bf0d94cebfdb8f8e4b
Ioan Ojica <ivan.andrei93@mail.ru>
2020-11-17 15:29:52 +0100

    we have the actions create and destroy for bookings

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,2 +1,24 @@
 class BookingController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to listings_path
+  end
+
+  def destroy
+    @booking = Booking.find(paramsp[:id])
+    @booking.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user.id, :listing.id)
+  end
 end
