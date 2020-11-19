1d7740db55ec3c322492efd650f84608991c0815
Ioan Ojica <ivan.andrei93@mail.ru>
2020-11-19 12:42:42 +0100

    dropdown category is done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_one :bookings
+
+SPORTS = [["Football"], ["Basketball"], ["Tennis"], ["Baseball"], ["American football"], ["Rugby"], ["Combat sports"], ["Volleyball"], ["Golf"], ["Other"]]
 end
+
