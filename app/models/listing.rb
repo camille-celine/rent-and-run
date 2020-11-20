936719d7277a9f8a02e114d58edd537641ce356e
Ioan Ojica <ivan.andrei93@mail.ru>
2020-11-20 13:01:08 +0100

    removed the validation for the foto in listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
   has_one :bookings
   has_one_attached :photo
 
-  validates :name, :description, :location, :price, :picture, :presence => true
+  validates :name, :description, :location, :price, :presence => true
   validates :availability, inclusion: { in: [true, false] }
 end
 
